87a462d3fcc2dad8d4fc0ca621a18fb574b98216
miyukiccina <80190927+miyukiccina@users.noreply.github.com>
2021-03-17 20:10:01 +0900

    first commit

---
+++ input.php
@@ -0,0 +1,31 @@
+<?php 
+ echo "牡羊座のあなたへ<br>";
+ echo "お疲れ様です<br>";
+
+
+    //     if(isset($_POST['seiza1'])) {
+    //     　  echo "牡羊座";
+    //     } else if(isset($_POST['seiza2'])) {
+    //     　  echo "牡牛座";
+    //     }else if(isset($_POST['seiza3'])) {
+    //     　  echo "双子座";
+    //      }else if(isset($_POST['seiza4'])) {
+    //     　  echo "蟹座";
+    //      }else if(isset($_POST['seiza5'])) {
+    //     　  echo "獅子座";
+    //      }else if(isset($_POST['seiza6'])) {
+    //     　  echo "乙女座";
+    //      }else if(isset($_POST['seiza7'])) {
+    //     　  echo "天秤座";
+    //     }else if(isset($_POST['seiza8'])) {
+    //     　  echo "蠍座";
+    //     }else if(isset($_POST['seiza9'])) {
+    //     　  echo "射手座";
+    //     }else if(isset($_POST['seiza10'])) {
+    //     　  echo "山羊座";
+    //      }else if(isset($_POST['seiza11'])) {
+    //     　  echo "水瓶座";
+    //     }else if(isset($_POST['seiza12'])) {
+    //     　  echo "魚座";
+    // }
+    ?>
